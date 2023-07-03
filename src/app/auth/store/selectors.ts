51964fcafc193a318f580f23175ebe1dc9b12322
s2000promax <s2000promax@gmail.com>
2023-07-03 12:49:26 +0400

    feat: add auth service

--- src/app/auth/store/selectors.ts
+++ src/app/auth/store/selectors.ts
@@ -1,5 +1,4 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { AppStateInterface } from '../../shared/types/appState.interface';
 import { AuthStateInterface } from '../types/authState.interface';
 
 export const authFeatureSelector =
